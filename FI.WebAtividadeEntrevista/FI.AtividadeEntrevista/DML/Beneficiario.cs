1733cdf5d6f6c0e8059c27834e2a8a84e7f82153
Alexandre Maximo <alexandre.maximoms@gmail.com>
2022-03-13 18:36:55 -0300

    Remoção do arquivo .gitignore que foi por engano e removeu o BD

---
+++ FI.WebAtividadeEntrevista/FI.AtividadeEntrevista/DML/Beneficiario.cs
@@ -0,0 +1,15 @@
+﻿namespace FI.AtividadeEntrevista.DML
+{
+    public class Beneficiario
+    {
+         /// <summary>
+        /// Nome
+        /// </summary>
+        public string Nome { get; set; }
+
+         /// <summary>
+        /// CPF
+        /// </summary>
+        public string CPF { get; set; }
+    }
+}
---
+++ FI.WebAtividadeEntrevista/FI.WebAtividadeEntrevista/Controllers/BeneficiarioController .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAtividadeEntrevista.Controllers
+{
+    public class BeneficiarioController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Beneficiáriro";
+
+            return View();
+        }
+    }
+}
